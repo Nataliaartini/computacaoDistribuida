824b3912d21e02f2153863558d8de33c927be6d0
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2023-04-04 21:48:10 -0300

    socket

--- socket.py
+++ socket.py
@@ -2,3 +2,15 @@
 
 HOST = 127.0.0.1
 # PORT = TEM QUE SER MAIOR QUE 1023 - SERÁ OUVIDA
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
