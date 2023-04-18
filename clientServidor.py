8238f3d5a6ac75528d03e3c538b5011409db15e3
Patrick Walter <patrick.2017316623@gmail.com>
2023-04-18 13:52:57 -0300

    cliente servidor

---
+++ clientServidor.py
@@ -0,0 +1,56 @@
+import socket
+import threading
+import sys
+
+HOST = "127.0.0.1"
+PORT1 = int(sys.argv[1])
+PORT2 = int(sys.argv[2])
+
+stream_lock = threading.Lock()
+
+def cliente():
+    stream_lock.acquire()
+    usuario = input("Digite seu nome: ")
+    stream_lock.release()
+    c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    c.connect((HOST, PORT2))
+
+    c.sendall(str.encode(usuario, "utf8"))
+    while True:
+        texto = input()
+        textoBytes = str.encode(texto, "utf8")
+        if not texto:
+            break
+        c.sendall(textoBytes)
+    c.close()
+
+def servidor():
+    try:
+        s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        s.bind((HOST,PORT1))
+        s.listen()
+        stream_lock.acquire()
+        print(f'Server rodando em {HOST}:{PORT1}')
+        stream_lock.release()
+
+        clientExternoNome = ''
+        client, address = s.accept()
+        while True:
+            receiveMessageFromClient = client.recv(1024).decode('utf8')
+            if clientExternoNome != '':
+                stream_lock.acquire()
+                print(f'{clientExternoNome}-> {receiveMessageFromClient}')
+                stream_lock.release()
+                if not receiveMessageFromClient:
+                    break
+                client.sendall(f'{clientExternoNome}-> {receiveMessageFromClient}'.encode('utf8'))
+            else:
+                clientExternoNome = receiveMessageFromClient
+        s.close()
+    except:
+        pass
+
+thread1 = threading.Thread(target=servidor,args=()) 
+thread2 = threading.Thread(target=cliente,args=())
+thread1.start()
+thread2.start()
