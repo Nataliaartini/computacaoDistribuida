9071e10d1be7fb806460d28968bae10ebcce37d1
Natalia Artini Ferrandin <79176834+Nataliaartini@users.noreply.github.com>
2023-04-25 21:36:44 -0300

    coments

--- v1/client.py
+++ v1/client.py
@@ -9,13 +9,20 @@ PORT = 1313
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 usuario = input("Digite seu nome: ")
+#fazer uma função pra definir o ID do usuário
 s.connect((HOST, PORT))
+
+def regiaoCritica():
+    isRegiaoCritica = 0
+    #implementar a função de eleição que decide qual processo(ID) vai rodar
 
 def enviar():
     while True:
         texto = input()
         textoBytes = str.encode(texto, "utf8")
         s.sendall(textoBytes)
+
+        #chamar a função de cima pra ver se pode acessar o server
 
 def receber():
     while True:
